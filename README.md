ad080f1b9bd86a5c9b13c7a44dbbe44ba8f0bcdb
inktrap <inktrap@inktrap.org>
2014-11-27 09:45:53 +0100

    release

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-xvfb-screenshooter
-==================
+# xvfb-screenshooter
 
-Take screenhosts and videos from your xvfb-session
+Take screenhosts and videos from your xvfb-session.
+
+xvfb-screenshooter is a wrapper to run and record arbitrary GUI programs.
+
+They will be run via  xvfb-run (which itself is a script to run stuff in Xvfb) and
+xvfb-screenshooter will record this via `avconv` and will take a screenshot with
+`xwd` each second. [Read more](https://inktrap.org/xvfb-screenshooter.html)
+
---
+++ xvfb-screenshooter.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -e
+
+# do not change this
+DISPLAY_BAK=$DISPLAY
+DISPLAY_SIZE=1600x900
+DEPTH=24
+
+LIMIT=0
+PAUSE=1
+TEST=0
+
+# do not change this
+DISPLAY_NUM=99
+SCREEN=0
+
+OUTPUT_DIR="./output"
+SC=0
+
+AUTH_ERR_LOG="./error.log"
+VIDEO_OUTFILE="${CMD}.ogv"
+IMAGE_OUTFILE="${CMD}.png"
+TMP="/tmp/${RANDOM}"
+
+# please change this :)
+# todo: $1
+CMD="./bin/helloworld.py"
+
+(rm "$AUTH_ERR_LOG" || true) 2>/dev/null
+(rm "$OUTPUT_DIR/$VIDEO_OUTFILE" || true) 2>/dev/null
+(rm "$OUTPUT_DIR/$IMAGE_OUTFILE" || true) 2>/dev/null
+
+while [[ $(pidof Xvfb) ]]; do
+    #echo "Instance of Xvfb already running. Behaviour undefined. Exiting."
+    #exit 1
+    echo "[*] Killing previous Xvfb instance"
+    killall Xvfb || true
+done
+
+function make_screenshot(){
+    if [ $SC = 0 ]; then
+        sleep 1
+    fi
+    SC=$((SC + 1))
+    echo "[*] Making Screenshot $SC"
+    (xwd -display :$DISPLAY_NUM -root -out "${TMP}${IMG}" >/dev/null && 
+        convert "${TMP}${IMG}" "${OUTPUT_DIR}/${SC}_${IMAGE_OUTFILE}") || true
+}
+
+echo "[*] Starting xvfb-run"
+(xvfb-run --error-file $AUTH_ERR_LOG -f "$HOME/.Xauthority" --server-args="-screen ${SCREEN} ${DISPLAY_SIZE}x${DEPTH}" "${CMD}" >/dev/null) & disown
+cmdpid=$!
+
+echo "[*] Recording"
+(avconv -v quiet -codec:a libvorbis -f x11grab -s $DISPLAY_SIZE -i :$DISPLAY_NUM -y "$OUTPUT_DIR/$VIDEO_OUTFILE" &>/dev/null) & disown
+recpid=$!
+
+while true; do
+    make_screenshot
+    sleep $PAUSE
+    # kill the recording after $limit iterations
+    if [[ ($SC -ge $LIMIT)  && ($LIMIT -ne 0) ]]; then
+        (kill "$recpid" >/dev/null) || true
+    elif [[ $TEST -eq 1 ]]; then
+        # kill the *window* (Hello World" after one iteration via xdotool
+        # windowkill this just means that if the application exits, xvfb shuts down
+        # and the recording stops
+        export DISPLAY=:${DISPLAY_NUM}
+        WID=$(xdotool search --name "Hello" | head -1)
+        xdotool windowkill "$WID"
+        export DISPLAY=${DISPLAY_BAK}
+    fi
+    (ps -p "$cmdpid" >/dev/null) || break
+done
+
+while true; do
+    kill "$recpid" 2>/dev/null || true
+    sleep $PAUSE
+    (ps -p "$recpid" >/dev/null) || break
+done
+(killall Xvfb 2>/dev/null || true)
+
+echo "[*] Finished cleanly"
+exit 0
+
+exit 0
